d44bc22bb2017a7ded1182832be6d77fe2708866
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2022-11-02 15:30:06 +0530

    fetch model from mlflow

--- jetson/fetch_model.py
+++ jetson/fetch_model.py
@@ -1,35 +1,94 @@
-from zenml.post_execution import get_pipeline
-from zenml.enums import ExecutionStatus
+"""Fetch model from MLFlow server."""
+import os
+
+import mlflow
 import onnx
 
 
-def fetch_onnx_model() -> bytes:
-    print("Fetching the model bytes from the ZenML server...")
-    pipeline = get_pipeline(pipeline="training_pipeline")
-    completed_runs = [run for run in pipeline.runs if run.status == ExecutionStatus.COMPLETED]
-    if len(completed_runs) == 0:
-        raise Exception("No completed runs found on ZenML server")
-    latest_run = completed_runs[-1]
-    onnx_step = latest_run.get_step(step="export_onnx")
-    onnx_bytes = onnx_step.output.read()
-    return onnx_bytes
+def print_artifact_info(artifact):
+    """Utility to print artifact information."""
+    print("artifact: {}".format(artifact.path))
+    print("is_dir: {}".format(artifact.is_dir))
+    print("size: {}".format(artifact.file_size))
 
 
-def load_model(onnx_bytes: bytes) -> onnx.ModelProto:
+def get_models_from_experiment_tracker(
+    tracking_uri: str, run_id: str, out_dir: str
+):
+    """Fetch model from mlflow server.
+
+    Args:
+        tracking_uri (str): Tracking uri for MLFlow server
+        run_id (str): Run ID for MLflow experiment
+        out_dir (str): Output directory path to save onnx model
+
+    Raises:
+        e: If `run_id` is invalid or not found in MLflow server
+    """
+    client = mlflow.tracking.MlflowClient(tracking_uri=tracking_uri)
+
+    if run_id != "":
+        print(
+            f"Choosing MLFlow Experiment with run id : {run_id} for deployment"
+        )
+        try:
+            run_data_dict = client.get_run(run_id).data.to_dictionary()
+            print(run_data_dict)
+        except Exception as e:
+            print(
+                f"Run id: {run_id} seems to be incorrect. Please check it again against MLFlow dashboard."
+            )
+            raise e
+
+        # list artifacts
+        artifacts = client.list_artifacts(run_id)
+        for artifact in artifacts:
+            print_artifact_info(artifact)
+
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+        # download onnx model
+        client.download_artifacts(run_id, "onnx_model", out_dir)
+
+
+def load_model(onnx_path: str):
+    """Load and check onnx model.
+
+    Args:
+        onnx_path (str): Path to onnx model (must end in `.onnx`)
+    """
     print("Checking ONNX model...")
+    onnx_model = onnx.load(onnx_path)
+    # Check the model
     try:
-        onnx_model = onnx.load_from_string(onnx_bytes)
         onnx.checker.check_model(onnx_model)
-    except Exception as e:
-        print(e) # TODO proper error handling
-        raise e
+    except onnx.checker.ValidationError as e:
+        print(f"The model is invalid: {e}")
+    else:
+        print("The model is valid!")
 
-    return onnx_model
+
+def fetch_onnx_from_mlflow(tracking_uri: str, run_id: str, out_dir: str):
+    """Fetch and load onnx model.
+
+    Args:
+        tracking_uri (str): Tracking uri for MLFlow server
+        run_id (str): Run ID for MLflow experiment
+        out_dir (str): Output directory path to save onnx model
+
+    Raises:
+        FileNotFoundError: If onnx model is not found at `{out_dir}/onnx_model/model.onnx` path
+    """
+    get_models_from_experiment_tracker(tracking_uri, run_id, out_dir)
+    onnx_path = f"{out_dir}/onnx_model/model.onnx"
+    if os.path.isfile(onnx_path):
+        load_model(onnx_path)
+    else:
+        raise FileNotFoundError(f"ONNX model not found at {onnx_path}")
 
 
-def fetch_onnx_from_zenml():
-    onnx_bytes = fetch_onnx_model()
-    onnx_model = load_model(onnx_bytes)
-
-    print("Saving ONNX model to a file...")
-    onnx.save_model(onnx_model, "model/dobble_model.onnx")
+# if __name__ == "__main__":
+#     tracking_uri = "http://localhost:5000"
+#     run_id = "d852ad88f6544b379f6e2d36b1436218"
+#     out_dir = "model"
+#     fetch_onnx_from_mlflow(tracking_uri, run_id, out_dir)
